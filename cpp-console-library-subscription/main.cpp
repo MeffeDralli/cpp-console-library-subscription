5e2a90738f1704bd8da1d5c4469a4571b14ae0f1
MeffeDralli <pristavkopolina29@gmail.com>
2024-06-06 23:18:27 +0300

    "Добавлен вывод общей информации о задании"

--- cpp-console-library-subscription/main.cpp
+++ cpp-console-library-subscription/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #4. GIT\n";
+    cout << "Variant #6. Roza Vetrov\n";
+    cout << "Author: Polina Pristavko <3 \n";
     return 0;
 }
