0bc1ead796444f7df27238c341a8daca7e4cd4dc
MeffeDralli <pristavkopolina29@gmail.com>
2024-06-06 23:45:12 +0300

    Добавлены тестовые данные

--- cpp-console-library-subscription/main.cpp
+++ cpp-console-library-subscription/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #4. GIT\n";
     cout << "Variant #6. Roza Vetrov\n";
     cout << "Author: Polina Pristavko <3 \n";
+    cout << "Group: XXIIIAI1d\n";
     return 0;
 }
